6a22240deb169f128671c8684f607810b990c934
micahzev <friedland.micah@gmail.com>
2018-09-15 18:58:48 +0200

    add appear transition

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import { CSSTransitionGroup } from 'react-transition-group'
 import "../styles.sass"
 
 export default () =>
-  <div>
-    <img src="/static/camaron.jpg" alt="CARLOTA" />
-  </div>
+  <CSSTransitionGroup
+      transitionName="hello"
+      transitionAppear={true}
+      transitionAppearTimeout={3000}
+      transitionEnter={false}
+      transitionLeave={false}>
+    <div>
+      <img src="/static/camaron.jpg" alt="CARLOTA" />
+    </div>
+  </CSSTransitionGroup>
